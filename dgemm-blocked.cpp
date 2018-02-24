a79a0c4bcf71607579144675a33975dc4b3ad9e7
mitchell-cody <35784238+mitchell-cody@users.noreply.github.com>
2018-02-24 18:09:56 -0500

    Update dgemm-blocked.cpp

--- dgemm-blocked.cpp
+++ dgemm-blocked.cpp
@@ -1,44 +1,216 @@
-#include <dgemm-blocked.hpp>
+#include <serial.hpp>
 
-const int BLOCK_SIZE = 41;
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <chrono>
+#include <fstream>
+#include <ios>
+#include <iostream>
+#include <vector>
 
-int min(const int &a, const int &b) { return (((a) < (b)) ? (a) : (b)); }
+#include <json.hpp>
 
-/* This auxiliary subroutine performs a smaller dgemm operation
- *  C := C + A * B
- * where C is M-by-N, A is M-by-K, and B is K-by-N. */
-void do_block(int lda, int M, int N, int K, double *A, double *B, double *C) {
-    /* For each row i of A */
-    for (int i = 0; i < M; ++i)
-        /* For each column j of B */
-        for (int j = 0; j < N; ++j) {
-            /* Compute C(i,j) */
-            double cij = C[i + j * lda];
-            for (int k = 0; k < K; ++k)
-                cij += A[i + k * lda] * B[k + j * lda];
-            C[i + j * lda] = cij;
-        }
-}
+#include <common.hpp>
 
-/* This routine performs a dgemm operation
- *  C := C + A * B
- * where A, B, and C are lda-by-lda matrices stored in column-major format.
- * On exit, A and B maintain their input values. */
-void square_dgemm_blocked(int lda, double *A, double *B, double *C) {
-    /* For each block-row of A */
-    for (int i = 0; i < lda; i += BLOCK_SIZE)
-        /* For each block-column of B */
-        for (int j = 0; j < lda; j += BLOCK_SIZE)
-            /* Accumulate block dgemms into block of C */
-            for (int k = 0; k < lda; k += BLOCK_SIZE) {
-                /* Correct block dimensions if block "goes off edge of" the matrix */
-                int M = min(BLOCK_SIZE, lda - i);
-                int N = min(BLOCK_SIZE, lda - j);
-                int K = min(BLOCK_SIZE, lda - k);
+//
+// Benchmarking program
+//
 
-                /* Perform individual block dgemm */
-                do_block(lda, M, N, K, A + i + k * lda, B + k + j * lda,
-                         C + i + j * lda);
+namespace serial {
+//
+// Serial implementation of simulation
+//
+  void serial(nlohmann::json &cli_parameters) {
+    int navg;
+    int nabsavg = 0;
+    double davg;
+    double dmin;
+    double absmin = 1.0;
+    double absavg = 0.0;
+
+    // Check number of particles for simulation
+    int n = cli_parameters["number_particles"];
+
+    // Check if output files should be saved.
+    std::string save_filename = cli_parameters["output_filename"];
+    std::string summary_filename = cli_parameters["summary_filename"];
+
+    // Open files (if specified)
+    std::ofstream fsave;
+    if (save_filename.empty()) {
+      fsave.open(nullptr);
+    } else {
+      fsave.open(save_filename);
+    }
+
+    std::ofstream fsum;
+    if (summary_filename.empty()) {
+      fsum.open(nullptr);
+    } else {
+      fsum.open(summary_filename, std::ios::app);
+    }
+
+    // Initialize particles for simulation
+    std::vector<particle_t> particles(static_cast<unsigned long>(n));
+    std::vector< std::vector<particle_t> > grid;
+    set_size(n);
+    init_particles(n, particles);
+    const double density = 0.0005;
+    const double mass = 0.01;
+    const double cutoff = 0.01;
+    const double min_r = (cutoff / 100);
+    const double dt = 0.0005;
+
+    double size = sqrt(density * n);
+
+        /*
+         * Set size of grid
+         * turn grid into 1 d array
+         * determine what bin particles are in
+         */
+    int rad_int = (2 * cutoff);
+    int grid_len = ceil(size/ rad_int);
+    grid.resize(grid_len*grid_len);
+
+    for (int i = 0; i < n; i++)
+    {
+      int px = particles[i].ax / rad_int;
+      int py = particles[i].ay / rad_int;
+      int p_bin = px*grid_len + py;
+      grid[p_bin].push_back(particles[i]);
+    }
+
+    std::vector<particle_t> get_neighbors(int p_bin)
+     {
+         std::vector<particle_t> neighbors;
+         for(int i = -1; i < 2; i++) {
+             for (int k = -1; k < 2; k++){
+              int bin = p_bin + (i * grid_len + k);
+              for(int j = 0; j < grid[bin].size(); j++) {
+                  neighbors.push_back(grid[bin][j]);
+              }
+              }
+          }
+         return neighbors;
+     }
+
+    // Disable checks and outputs?
+    bool disable_checks = cli_parameters["disable_checks"];
+
+    //
+    // Simulate a number of time steps
+    //
+
+    auto timer = read_timer();
+
+    for (int step = 0; step < NSTEPS; step++) {
+      navg = 0;
+      davg = 0.0;
+      dmin = 1.0;
+
+      //
+      // Compute forces
+      //
+      /*
+       * Loop over bins instead of all particles
+       * loop over x and y dimensions of the grid
+       * loop over all particles in the chosen grid
+       *
+       */
+      for (int i = 0; i < grid_len; i++) {
+        for (int j = 0; j < grid_len; j++) {
+          int part_bin = i*grid_len+j;
+          std::vector<particle_t> neighbors = get_neighbors(part_bin);
+          for (int parts_in_bin = 0; parts_in_bin < grid[part_bin].size(); parts_in_bin++) {
+            grid[part_bin][parts_in_bin].ax = 0;
+            grid[part_bin][parts_in_bin].ay = 0;
+            for (int adj = 0; adj < neighbors.size(); adj++) {
+              apply_force(particles[i], particles[j], dmin, davg, navg);
             }
-}
+          }
+        }
+      }
 
+      //need to account for particles already used
+
+      //
+      // Move particles
+      //
+      for (int i = 0; i < n; i++) {
+        move(particles[i]);
+      }
+
+      //near to clear and rebind
+      for (int i = 0, i < grid_len*grid_len; i++){
+        grid[i].clear()
+      }
+      for (int i = 0; i < n; i++)
+      {
+        int px = particles[i].ax / rad_int;
+        int py = particles[i].ay / rad_int;
+        int p_bin = px*grid_len + py;
+        grid[p_bin].push_back(particles[i]);
+      }
+
+
+      if (!disable_checks) {
+        //
+        // Computing statistical data
+        //
+        if (navg) {
+          absavg += davg / navg;
+          nabsavg++;
+        }
+        if (dmin < absmin) {
+          absmin = dmin;
+        }
+
+        //
+        // Save if necessary
+        //
+        if (fsave && (step % SAVEFREQ) == 0) {
+          save(fsave, n, particles);
+        }
+      }
+    }
+
+    auto simulation_time =
+    std::chrono::duration<double, std::micro>(read_timer() - timer).count()
+    / 1e6;
+
+    std::cout << "n = " << n << ", simulation time = " << simulation_time
+              << " seconds";
+
+    if (!disable_checks) {
+      if (nabsavg) {
+        absavg /= nabsavg;
+      }
+      // - the minimum distance absmin between 2 particles during the run of
+      //   the simulation
+      // - A Correct simulation will have particles stay at greater than 0.4
+      //   (of cutoff) with typical values between .7-.8
+      // - A simulation were particles don't interact correctly will be less
+      //   than 0.4 (of cutoff) with typical values between .01-.05
+      // - The average distance absavg is ~.95 when most particles are
+      //   interacting correctly and ~.66 when no particles are interacting
+      std::cout << ", absmin = " << absmin << ", absavg = " << absavg;
+      if (absmin < 0.4) {
+        std::cout << "\nThe minimum distance is below 0.4 meaning that some "
+                "particle is not interacting";
+      }
+      if (absavg < 0.8) {
+        std::cout << "\nThe average distance is below 0.8 meaning that most "
+                "particles are not interacting";
+      }
+    }
+    std::cout << "\n";
+
+    //
+    // Printing summary data
+    //
+    if (fsum) {
+      fsum << n << " " << simulation_time << "\n";
+    }
+  }
+}
